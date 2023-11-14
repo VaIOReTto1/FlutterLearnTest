3de7dd785dc202be10e05bc5e933ef3b55b158ab
VaIOReTto1 <1302140648@qq.com>
2023-11-14 23:23:45 +0800

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dynamic Form Validation',
+      home: DynamicFormValidation(),
+    );
+  }
+}
+
+class DynamicFormValidation extends StatefulWidget {
+  @override
+  _DynamicFormValidationState createState() => _DynamicFormValidationState();
+}
+
+class _DynamicFormValidationState extends State<DynamicFormValidation> {
+  late TextEditingController textController;
+  late bool isButtonEnabled;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+    isButtonEnabled = false;
+
+    // TODO: 为文本框添加监听器，当文本框内容发生变化时，更新按钮状态
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomePage(),
-    )
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+      appBar: AppBar(
+        title: Text('Dynamic Form Validation'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                labelText: 'Enter some text',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: isButtonEnabled ? () {
+              // TODO: 有文本时，提交表单
+            } : null,
+            child: Text('Submit'),
+          ),
+        ],
+      ),
+    );
   }
 }
