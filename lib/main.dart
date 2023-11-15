6833dfd96aa7f18f2c3abac922cff9d63c83c2fb
VaIOReTto1 <1302140648@qq.com>
2023-11-15 16:12:40 +0800

    optimize

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: '计数器应用',
+      debugShowCheckedModeBanner: false,
       home: CounterPage(),
     );
   }
