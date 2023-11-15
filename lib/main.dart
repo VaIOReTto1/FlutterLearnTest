b9e43f961294de4b86192c1f47d4789dd9083acb
VaIOReTto1 <1302140648@qq.com>
2023-11-15 15:22:20 +0800

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TodosProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Dynamic Form Validation',
-      home: DynamicFormValidation(),
+      title: 'Todo App',
+      home: TodoListScreen(),
     );
   }
 }
 
-class DynamicFormValidation extends StatefulWidget {
-  @override
-  _DynamicFormValidationState createState() => _DynamicFormValidationState();
-}
-
-class _DynamicFormValidationState extends State<DynamicFormValidation> {
-  late TextEditingController textController;
-  late bool isButtonEnabled;
-
-  @override
-  void initState() {
-    super.initState();
-    textController = TextEditingController();
-    isButtonEnabled = false;
-
-    // TODO: 为文本框添加监听器，当文本框内容发生变化时，更新按钮状态
-  }
-
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
-  }
-
+class TodoListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dynamic Form Validation'),
-      ),
+      appBar: AppBar(title: Text('Todo List')),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: textController,
-              decoration: InputDecoration(
-                labelText: 'Enter some text',
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: isButtonEnabled ? () {
-              // TODO: 有文本时，提交表单
-            } : null,
-            child: Text('Submit'),
-          ),
+        children: [
+          // TODO: 实现显示 Todo 列表的 Widget
+
+          // TODO: 实现添加新 Todo 的 Widget
         ],
       ),
     );
   }
+}
+
+class TodoModel {
+  String title;
+  bool isDone;
+
+  TodoModel({required this.title, this.isDone = false});
+
+// TODO: 添加方法来切换任务的完成状态
+
+}
+
+class TodosProvider extends ChangeNotifier {
+  List<TodoModel> _todos = [];
+
+  List<TodoModel> get todos => _todos;
+
+// TODO: 实现添加 Todo 的方法
+
+// TODO: 实现切换 Todo 完成状态的方法
+
+// 当添加或修改 Todo 时，调用 notifyListeners() 来通知监听者
 }
