0c7fd057898295d1cdf5b8b04f02d9f23237685d
VaIOReTto1 <1302140648@qq.com>
2023-11-15 16:06:58 +0800

    optimize

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: '计数器应用',
       home: CounterPage(),
     );
@@ -15,6 +17,8 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
   @override
   _CounterPageState createState() => _CounterPageState();
 }
@@ -30,26 +34,32 @@ class _CounterPageState extends State<CounterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('计数器应用'),
+        title: const Text('计数器应用'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
+          children: [
+            const Text(
               '你已经按下按钮这么多次了:',
+            ),
+            const SizedBox(
+              height: 5,
             ),
             Text(
               '$_counter', // 这里将显示计数器的值
-              style: Theme.of(context).textTheme.headline4,
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter, // 当按钮被按下时，_incrementCounter 方法将被调用
+              tooltip: '增加',
+              child: const Icon(Icons.add),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter, // 当按钮被按下时，_incrementCounter 方法将被调用
-        tooltip: '增加',
-        child: Icon(Icons.add),
       ),
     );
   }
