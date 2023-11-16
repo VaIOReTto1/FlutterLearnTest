22580c9cafd105c20b0e8b877cbeea10678d04a3
VaIOReTto1 <1302140648@qq.com>
2023-11-16 17:09:56 +0800

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,63 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      title: 'Scroll Listener Example',
+      home: ScrollListenerExample(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class ScrollListenerExample extends StatefulWidget {
+  const ScrollListenerExample({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: IconButton(
-          icon: Icon(Icons.chevron_right),
-          onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyStatePage()));
-          },
-        ),
-      ),
-    );
-  }
+  _ScrollListenerExampleState createState() => _ScrollListenerExampleState();
 }
 
-
-class MyStatePage extends StatefulWidget {
-  const MyStatePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatePage> createState() => _MyStatePageState();
-}
-
-class _MyStatePageState extends State<MyStatePage> {
-
+class _ScrollListenerExampleState extends State<ScrollListenerExample> {
+  final ScrollController _scrollController = ScrollController();
+  double _offset = 0.0;
 
   @override
   void initState() {
     super.initState();
-    print("初始化：initState");
+    _scrollController.addListener(_scrollListener);
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print("依赖变化：didChangeDependencies");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("构建页面：build");
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print("调用setState重新build");
-            setState(() {});
-          },
-          icon: Icon(Icons.ac_unit,),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    print("热重载被调用，用于调试：reassemble");
-  }
-
-  @override
-  void didUpdateWidget(MyStatePage oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("组件被重新构建：didUpdateWidget");
-  }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-    print("组件被移除节点：deactivate");
+  void _scrollListener() {
+    setState(() {
+      _offset = _scrollController.offset;
+    });
   }
 
   @override
   void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
     super.dispose();
-    print("永久移除组件：dispose");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scroll Listener Example'),
+      ),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text('Item$index'));
+        },
+      ),
+      bottomNavigationBar: Text('Scroll Offset: $_offset'),
+    );
   }
 }
