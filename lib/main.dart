5852ad5d29217460b8258aae270c31747df0ac38
VaIOReTto1 <1302140648@qq.com>
2023-11-16 17:06:04 +0800

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(home: MyApp()));
+main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Demo'),
-          ),
-          body: const HomePage(),
-        ));
+      home: HomePage(),
+    );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("状态管理"),
+      ),
+      body: const Center(
+        child: LikeIcon(),
+      ),
+    );
+  }
+}
+
+class LikeIcon extends StatefulWidget {
+  const LikeIcon({Key? key}) : super(key: key);
+
+  @override
+  State<LikeIcon> createState() => _LikeIconState();
+}
+
+class _LikeIconState extends State<LikeIcon> {
+  bool ok = false; // true代表点赞，false代表没点赞
+
+  clickFunc() {
+    // 使用State类提供的setState函数来刷新Widget状态
+    setState(() {
+      ok = !ok;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () => clickFunc(),
+        child: Icon(
+          Icons.thumb_up,
+          color: ok ? Colors.blue : Colors.grey,
+        ));
   }
 }
