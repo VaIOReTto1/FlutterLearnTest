173ad1afb03c5ab69ebb9369179f1c9f6b0ca984
VaIOReTto1 <1302140648@qq.com>
2023-11-18 19:00:49 +0800

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,66 +9,131 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DynamicFormValidation(),
+      home: Scaffold(appBar: AppBar(), body: const ExpandedContanier(ExpandedContent: ExpandedContent(),ExpandedTitle: ExpandedTitle(),)),
     );
   }
 }
 
-class DynamicFormValidation extends StatefulWidget {
-  const DynamicFormValidation({super.key});
+class ExpandedContanier extends StatefulWidget {
+  final Widget ExpandedContent;
+  final Widget ExpandedTitle;
+  const ExpandedContanier({super.key,required this.ExpandedContent, required this.ExpandedTitle});
 
   @override
-  _DynamicFormValidationState createState() => _DynamicFormValidationState();
+  State<ExpandedContanier> createState() => _ExpandedContanierState();
 }
 
-class _DynamicFormValidationState extends State<DynamicFormValidation> {
-  late TextEditingController textController;
-  late bool isButtonEnabled;
-
-  @override
-  void initState() {
-    super.initState();
-    textController = TextEditingController();
-    isButtonEnabled = false;
-
-    // TODO: 为文本框添加监听器，当文本框内容发生变化时，更新按钮状态
-  }
-
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
-  }
+class _ExpandedContanierState extends State<ExpandedContanier> {
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('输入框监听'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: textController,
-              decoration: const InputDecoration(
-                labelText: '输入文本',
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: const BorderRadius.all(Radius.circular(8))),
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // 使用MainAxisSize.min
+          children: [
+            Row(
+              children: [
+               widget.ExpandedTitle,
+                Expanded(child: Container()),
+                InkWell(
+                  child: Icon(_isExpanded
+                      ? Icons.keyboard_arrow_up_outlined
+                      : Icons.keyboard_arrow_down_outlined),
+                  onTap: () {
+                    setState(() {
+                      _isExpanded = !_isExpanded;
+                    });
+                  },
+                )
+              ],
             ),
+            Visibility(
+              visible: _isExpanded, // 根据_isExpanded的值显示或隐藏
+              child: widget.ExpandedContent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExpandedTitle extends StatelessWidget {
+  const ExpandedTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      height: 50,
+      child: const Row(
+        children: [
+          Icon(Icons.person),
+          SizedBox(
+            width: 8,
           ),
-          ElevatedButton(
-            onPressed: isButtonEnabled
-                ? () {
-                    // TODO: 有文本时，提交表单
-                  }
-                : null,
-            child: const Text('提交'),
+          Text(
+            "个性化",
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ],
+      ),
+    );
+  }
+}
+
+
+class ExpandedContent extends StatelessWidget {
+  const ExpandedContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: const BorderRadius.all(Radius.circular(8))),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Icon(Icons.person),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text("更多信息"),
+                  Expanded(child: Container()),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Icon(Icons.person),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text("更多信息"),
+                  Expanded(child: Container()),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
